b90c50072e810ba806ef9b7a63df89e1d5339f0d
Matheus Pazinati <matheuspazinati@gmail.com>
2021-11-18 10:18:25 -0300

    Organiza codigo JS

--- main.js
+++ main.js
@@ -7,15 +7,40 @@ const Modal = {
     document.querySelector('[data-modal]').classList.remove('active')
   }
 }
+const Utils = {//Pequenas funções de controle e tratamento de dados da aplicação
+  clearFields() {//Limpa os campos do modal após ser submetido;
+    const formInputs = document.querySelectorAll('.form-data input')
+    formInputs.forEach((input) => {
+    input.value = "";
+    })
+    const radioIncome = document.querySelector('input#income')
+    radioIncome.checked = true;
+  },
 
-function typeOfTransaction() { //Verifica o tipo da operação, se é Entrada ou Saída através dos radio buttons
-  const operation = document.querySelector('input#income')
-  if (operation.checked) {
-    return operation.id //Retorna "income"
-  } else {
-    return "expense"
+  transformCurrency(amount) { //Transforma o valor capturado no input e transforma em formato de dinheiro
+    amount = Number(amount).toLocaleString('pt-BR', {
+    style: 'currency',
+    currency: 'BRL'
+    })
+    return amount;
+  },
+
+  transformDate(date) {//Altera a visualização da data para o formato BR
+    const updatedDate = date.split("-")
+    date = `${updatedDate[2]}/${updatedDate[1]}/${updatedDate[0]}`
+    return date
+  },
+
+  typeOfTransaction() {//Verifica o tipo da operação, se é Entrada ou Saída através dos radio buttons
+    const operation = document.querySelector('input#income')
+    if (operation.checked) {
+      return operation.id //Retorna "income"
+    } else {
+      return "expense"
+    }
   }
 }
+
 const transactions = [];
 const Form = {
   submit(event) {
@@ -24,36 +49,14 @@ const Form = {
       description: document.querySelector('input#description').value,
       amount: document.querySelector('input#value').value,
       date: document.querySelector('input#date').value,
-      type: typeOfTransaction(),
+      type: Utils.typeOfTransaction(),
     }
     transactions.push(transaction)
     innerTransaction()
     Balance.updateBalance(transaction)
-    clearFields()
+    Utils.clearFields()
     Modal.close()
   }
-}
-function clearFields(){//Limpa os campos do form após serem submetidos
-  const formInputs = document.querySelectorAll('.form-data input')
-  formInputs.forEach((input) => {
-    input.value = "";
-  })
-  const radioIncome = document.querySelector('input#income')
-  radioIncome.checked = true;
-}
-
-function transformCurrency(amount) { //Transforma o valor capturado no input e transforma em formato de dinheiro
-  amount = Number(amount).toLocaleString('pt-BR', {
-    style: 'currency',
-    currency: 'BRL'
-  })
-  return amount;
-}
-
-function transformDate(date) {//Altera a visualização da data para o formato BR
-  const updatedDate = date.split("-")
-  date = `${updatedDate[2]}/${updatedDate[1]}/${updatedDate[0]}`
-  return date
 }
 
 function removeTransaction(index) {//Remove a linha da transação quando o botão de excluir for clicado
@@ -75,8 +78,8 @@ function innerTransaction() {
   transactions.forEach((transaction, index) => {
     tr.dataset.index = index;
     tr.innerHTML = `<td class="table-body-description">${transaction.description}</td>
-      <td class="table-body-value ${transaction.type}">${transformCurrency(transaction.amount)}</td>
-      <td class="table-body-date">${transformDate(transaction.date)}</td>
+      <td class="table-body-value ${transaction.type}">${Utils.transformCurrency(transaction.amount)}</td>
+      <td class="table-body-date">${Utils.transformDate(transaction.date)}</td>
       <td class="table-icon-remove"><img onclick="removeTransaction(${index})" src="./assets/images/minus.svg" alt="Ícone de remover"></td>`;
     }
   )
@@ -89,7 +92,7 @@ let Balance = {
   cardIncome: document.querySelector('[data-card-income]'),
   cardExpense: document.querySelector('[data-card-expense]'),
   cardTotal: document.querySelector('[data-card-total]'),
-  updateBalance(transaction) {
+  updateBalance(transaction) {//Atualiza os valores do balanço, verificando tipo da transação
     if (transaction.type == "income") {
       Balance.updateIncomes(Number(transaction.amount))
     } else {
@@ -103,7 +106,7 @@ let Balance = {
   updateExpenses(value) {
     Balance.expenses += value;
   },
-  updateTotal() {
+  updateTotal() {//Calcula o total do balanço e aplica a classe no container dependendo do resultado (negativo ou positivo)
     const containerTotal = Balance.cardTotal.parentElement
     const total =  Balance.incomes - Balance.expenses
     if (total < 0 ) {
@@ -116,9 +119,10 @@ let Balance = {
     return total;
   }
 }
+
 function innerBalanceValues() { //Função para inserir no HTML os valores do balanço
-  Balance.cardIncome.textContent = transformCurrency(Balance.incomes)
-  Balance.cardExpense.textContent = transformCurrency(Balance.expenses)
-  Balance.cardTotal.textContent = transformCurrency(Balance.updateTotal())
+  Balance.cardIncome.textContent = Utils.transformCurrency(Balance.incomes)
+  Balance.cardExpense.textContent = Utils.transformCurrency(Balance.expenses)
+  Balance.cardTotal.textContent = Utils.transformCurrency(Balance.updateTotal())
 }
 innerBalanceValues()//Neste momento, os valores vão estar zerados
